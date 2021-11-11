b4bb095a086eb310246a129eaf7d98c332a93375
tunglam268 <73482349+tunglam268@users.noreply.github.com>
2021-11-11 08:47:34 +0700

    Add files via upload

---
+++ SHA256Example/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+
+	secret := "mysecret"
+	data := "data"
+	fmt.Printf("Secret: %s Data: %s\n", secret, data)
+
+	// Create a new HMAC by defining the hash type and the key (as byte array)
+	h := hmac.New(sha256.New, []byte(secret))
+
+	// Write Data to it
+	h.Write([]byte(data))
+
+	// Get result and encode as hexadecimal string
+	sha := hex.EncodeToString(h.Sum(nil))
+
+	fmt.Println("Result: " + sha)
+}
